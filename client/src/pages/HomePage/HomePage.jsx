5418c67b134c29933eb6ba4acca6696cbc2beb58
safonov_dmitriy <sdssds341@gmail.com>
2021-10-28 14:46:36 +0300

    [refactor] Home Page show or not

--- client/src/pages/HomePage/HomePage.jsx
+++ client/src/pages/HomePage/HomePage.jsx
@@ -1,16 +1,17 @@
 import React, { useEffect } from "react";
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import styled from "styled-components";
 import { userFetch } from "../../redux/actions/userAction";
+import { userLoadingSelector } from "../../redux/selectors";
 const HomePageWrapper = styled.div``;
 const LunchMenuWrapper = styled.div``;
 const HomePage = () => {
   const dispatch = useDispatch();
-
+  const userLoading = useSelector(userLoadingSelector);
   useEffect(() => {
     dispatch(userFetch());
   }, [dispatch]);
 
-  return <HomePageWrapper></HomePageWrapper>;
+  return !userLoading && <HomePageWrapper></HomePageWrapper>;
 };
 export default HomePage;
--- client/src/redux/selectors/index.js
+++ client/src/redux/selectors/index.js
@@ -1,3 +1,4 @@
 export const userRoleSelector = (state) => state.user.userData.role;
 export const userEmailSelector = (state) => state.user.userData.email;
 export const userBalanceSelector = (state) => state.user.userData.balance;
+export const userLoadingSelector = (state) => state.user.loading;
