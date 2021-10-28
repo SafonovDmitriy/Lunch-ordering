de791dd5f4a2f40879cd027a05759174f2cf905e
safonov_dmitriy <sdssds341@gmail.com>
2021-10-28 15:13:26 +0300

    [feat] lanchMenu endPoints

--- server/routes/index.js
+++ server/routes/index.js
@@ -2,10 +2,12 @@ const Router = require("express");
 const router = new Router();
 const userRouter = require("./userRouter");
 const authRouter = require("./authRouter.js");
+const lanchMenuRouter = require("./lanchMenuRouter");
 
 const { checkToken } = require("../middlewares");
 
 router.use("/auth", authRouter);
 router.use("/user", checkToken, userRouter);
+router.use("/lanchMenu", checkToken, lanchMenuRouter);
 
 module.exports = router;
