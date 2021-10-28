e71fd735cd9fea92385e48607b810d58f1e40dfc
safonov_dmitriy <sdssds341@gmail.com>
2021-10-28 13:42:49 +0300

    [feat] fetch User Data

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,15 @@
+import { useEffect } from "react";
+import { useDispatch } from "react-redux";
 import { RootRouter } from "./components/Router";
+import { userFetch } from "./redux/actions/userAction";
 
 const App = () => {
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(userFetch());
+  }, [dispatch]);
+
   return <RootRouter />;
 };
 
