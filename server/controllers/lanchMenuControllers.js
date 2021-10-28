40f1340c29534fe960d4cfaf716965a227bf0c8a
safonov_dmitriy <sdssds341@gmail.com>
2021-10-28 16:28:25 +0300

    [feat] get Lanch Menu end point

--- server/controllers/lanchMenuControllers.js
+++ server/controllers/lanchMenuControllers.js
@@ -1,5 +1,13 @@
+const { LanchMenu } = require("../models");
+
 class LanchMenuController {
-  async getAllLanch(req, res) {}
+  async getAllLanch(req, res) {
+    const lanchMenu = await LanchMenu.find({});
+    res.status(200).json({
+      message: "That's all the lanch menu what is in the database",
+      lanchMenu,
+    });
+  }
   async getLanchById(req, res) {}
   async createNewLanchMenu(req, res) {}
   async updateLanchMenuById(req, res) {}
