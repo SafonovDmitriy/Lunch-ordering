cd139b1938827b1f9aa07c9d65a9e1d18f9091f9
safonov_dmitriy <sdssds341@gmail.com>
2021-11-05 13:35:12 +0200

    [refactor] passport-setup

--- server/config/passport-setup.js
+++ server/config/passport-setup.js
@@ -1,5 +1,4 @@
 const JwtStrategy = require("passport-jwt").Strategy;
-const { User } = require("../models");
 const ExtractJwt = require("passport-jwt").ExtractJwt;
 
 const cookieExtractor = ({ cookies: { token } }) => {
@@ -13,16 +12,6 @@ const jwtOptions = {
 
 const jwtStrategy = new JwtStrategy(jwtOptions, ({ _id }, done) => {
   done(null, { userId: _id });
-  // User.findById(_id, (err, user) => {
-  //   if (err) {
-  //     return done(err, false);
-  //   }
-  //   if (user) {
-  //     return done(null, user);
-  //   } else {
-  //     return done(null, false);
-  //   }
-  // });
 });
 
 module.exports = (passport) => {
