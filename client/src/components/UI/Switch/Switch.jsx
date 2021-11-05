eacd65f660146c1f1168c7db1bddb17af8f7727c
safonov_dmitriy <sdssds341@gmail.com>
2021-11-05 09:18:49 +0200

    [refactor] Switch Styles

--- client/src/components/UI/Switch/Switch.jsx
+++ client/src/components/UI/Switch/Switch.jsx
@@ -3,25 +3,20 @@ import React from "react";
 import styled from "styled-components";
 const SwitchModeContainer = styled.div`
   width: 50px;
-
-  display: flex;
-  justify-content: center;
-  align-items: center;
+  position: relative;
 `;
 const SwitchMode = styled.div`
   height: 70px;
   width: 35px;
   border: solid 1px;
   border-radius: 25px;
-  display: flex;
-  justify-content: center;
   box-sizing: border-box;
   padding: 2px 0;
-  align-items: ${({ mode }) => (mode === "true" ? "flex-end" : "flex-start")};
   background-color: ${({ mode }) => (mode === "true" ? "green" : "red")};
   transition: all 0.3s;
-  animation: 0.03s
-    ${({ mode }) => (mode === "true" ? "ball-move-up" : "ball-move-down")};
+  position: fixed;
+  top: 50%;
+  left: 5px;
 `;
 const Ball = styled.div`
   height: 25px;
@@ -29,12 +24,17 @@ const Ball = styled.div`
   border: solid 1px;
   border-radius: 25px;
   background-color: white;
+  position: absolute;
+  left: 3px;
+  animation: 0.03s
+    ${({ mode }) => (mode === "true" ? "ball-move-up" : "ball-move-down")};
+  top: ${({ mode }) => (mode === "true" ? "3%" : "57%")}; ;
 `;
 const Switch = ({ mode, setModeHendler }) => {
   return (
     <SwitchModeContainer>
       <SwitchMode mode={mode.toString()} onClick={setModeHendler}>
-        <Ball />
+        <Ball mode={mode.toString()} />
       </SwitchMode>
     </SwitchModeContainer>
   );
