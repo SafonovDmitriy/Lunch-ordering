39644d16331337ace9174850352249b66236ce80
safonov_dmitriy <sdssds341@gmail.com>
2021-11-05 09:18:24 +0200

    [refactor] Loading

--- client/src/components/Loading/Loading.jsx
+++ client/src/components/Loading/Loading.jsx
@@ -1,7 +1,20 @@
 import React from "react";
-
-const Loading = () => {
-  return <div>Loading...</div>;
-};
+import ClipLoader from "react-spinners/ClipLoader";
+import styled from "styled-components";
+const LoadingContainer = styled.div`
+  position: absolute;
+  top: 0;
+  bottom: 0;
+  left: 0;
+  right: 0;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+const Loading = () => (
+  <LoadingContainer>
+    <ClipLoader color={"red"} loading={true} size={150} />
+  </LoadingContainer>
+);
 
 export default Loading;
