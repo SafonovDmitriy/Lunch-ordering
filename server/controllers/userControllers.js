53c8aef341e6558d33c21ce2487167f96d6da074
safonov_dmitriy <sdssds341@gmail.com>
2021-11-08 17:35:37 +0200

    [feat] userServices

--- server/controllers/userControllers.js
+++ server/controllers/userControllers.js
@@ -1,9 +1,9 @@
-const { User } = require("../models");
+const userServices = require("../services/userServices");
 
 class UserController {
   async getUser(req, res) {
     const { userId } = req.user;
-    const user = await User.findById({ _id: userId }, { password: 0 });
+    const user = await userServices.findUserById({ userId });
     res.status(200).json({ user });
   }
 
---
+++ server/services/userServices.js
@@ -0,0 +1,57 @@
+const { User } = require("../models");
+const Bcrypt = require("../utils/Bcrypt");
+const Token = require("../utils/Token");
+
+class UserServices {
+  async getUsers({ limit, page }) {
+    const users = await this.getUsersWithPagination({ limit, page });
+    const totalPage = await this.getTotalPagesForUsers({ limit });
+    return { users, totalPage };
+  }
+
+  async getUsersWithPagination({ limit, page }) {
+    return await User.find(
+      {},
+      { password: 0, role: 0, photo: 0 },
+      {
+        limit: Number(limit),
+        skip: limit * page,
+      }
+    );
+  }
+
+  async getTotalPagesForUsers({ limit }) {
+    const usersLength = await User.countDocuments();
+    return Math.ceil(usersLength / limit);
+  }
+
+  async updateBalanceForUsers({ selectUserId: _id, balance, userId }) {
+    await User.findByIdAndUpdate(_id, { balance });
+    return { mainUser: _id === userId ? { balance } : null };
+  }
+  async findUserByEmail({ email }) {
+    return await User.findOne({ email });
+  }
+  async findUserById({ userId }) {
+    return User.findById(userId, { password: 0 });
+  }
+  async createNewUser({ email, password }) {
+    const hashPassword = await new Bcrypt({ password }).hash();
+    const verifyCode = new Token().createEmptyToken();
+    const user = new User({
+      email,
+      password: hashPassword,
+      role: "USER",
+      photo: "img/avatar.png",
+      balance: 0,
+      verifyCode,
+    });
+    await user.save();
+    console.log(urlForVerification(email));
+    console.log(`verifyCode`, verifyCode);
+  }
+  async verifyUser({ email }) {
+    await User.updateOne({ email }, { $unset: { verifyCode: 1 } });
+  }
+}
+module.exports = new UserServices();
