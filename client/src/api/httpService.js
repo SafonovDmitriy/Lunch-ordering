d8489c4f34b7a63f4c63f7ca8540ac6a1c4ed580
safonov_dmitriy <sdssds341@gmail.com>
2021-11-09 11:54:47 +0200

    [refactor] httpService

--- client/src/api/httpService.js
+++ client/src/api/httpService.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-export const instance = axios.create({
+const instance = axios.create({
   baseURL: process.env.REACT_APP_URL_SERVER,
   headers: {
     "Content-Type": "application/json",
