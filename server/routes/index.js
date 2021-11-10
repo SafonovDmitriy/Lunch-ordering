9a697f78c3a6e198632af2211e87f97129844c9e
safonov_dmitriy <sdssds341@gmail.com>
2021-11-10 13:03:26 +0200

    [refactor] index routes

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,42 +1,52 @@
 const Router = require("express");
-const router = new Router();
 const userRouter = require("./userRouter");
-const authRouter = require("./authRouter.js");
+const authRouter = require("./authRouter");
 const lunchMenuRouter = require("./lunchMenuRouter");
-const dishRouter = require("./dishRouter.js");
-const orderRouter = require("./orderRouter.js");
-const adminRouter = require("./adminRouter.js");
-
+const dishRouter = require("./dishRouter");
+const orderRouter = require("./orderRouter");
+const adminRouter = require("./adminRouter");
 const { checkUserRole } = require("../middlewares");
 const passport = require("passport");
 
+const router = new Router();
+const protectedRouter = new Router();
+
+protectedRouter.use(passport.authenticate("jwt", { session: false }));
+protectedRouter.use("/user", userRouter);
+protectedRouter.use("/lunch-menu", lunchMenuRouter);
+protectedRouter.use("/dish", checkUserRole, dishRouter);
+protectedRouter.use("/user-order-history", orderRouter);
+protectedRouter.use("/admin", checkUserRole, adminRouter);
+
 router.use("/auth", authRouter);
-router.use(
-  "/user",
-  passport.authenticate("jwt", { session: false }),
-  userRouter
-);
-router.use(
-  "/lunch-menu",
-  passport.authenticate("jwt", { session: false }),
-  lunchMenuRouter
-);
-router.use(
-  "/dish",
-  passport.authenticate("jwt", { session: false }),
-  checkUserRole,
-  dishRouter
-);
-router.use(
-  "/user-order-history",
-  passport.authenticate("jwt", { session: false }),
-  orderRouter
-);
-router.use(
-  "/admin",
-  passport.authenticate("jwt", { session: false }),
-  checkUserRole,
-  adminRouter
-);
+router.use(protectedRouter);
+
+// router.use(
+//   "/user",
+//   passport.authenticate("jwt", { session: false }),
+//   userRouter
+// );
+// router.use(
+//   "/lunch-menu",
+//   passport.authenticate("jwt", { session: false }),
+//   lunchMenuRouter
+// );
+// router.use(
+//   "/dish",
+//   passport.authenticate("jwt", { session: false }),
+//   checkUserRole,
+//   dishRouter
+// );
+// router.use(
+//   "/user-order-history",
+//   passport.authenticate("jwt", { session: false }),
+//   orderRouter
+// );
+// router.use(
+//   "/admin",
+//   passport.authenticate("jwt", { session: false }),
+//   checkUserRole,
+//   adminRouter
+// );
 
 module.exports = router;
