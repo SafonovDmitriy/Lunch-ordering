c9fd83c8ab72559ee6da1e867cd695484e4b2013
safonov_dmitriy <sdssds341@gmail.com>
2021-11-12 11:20:36 +0200

    [feat] contants validation Messages

--- client/src/constants/index.js
+++ client/src/constants/index.js
@@ -16,7 +16,9 @@ export const STATUS_ERRORS = {
   UNAUTHORIZED: 401,
 };
 export const VALIDATION_MASSAGES = {
-  REQUIRED: ({ name }) => `The ${name} field must be filled`,
+  REQUIRED: (name) => `The ${name} field must be filled`,
   VALIDATION_EMAIL: "In this field there must be an existing email",
-  SAME_FIELD: "Confirming password does not meet the original",
+  SAME_FIELD: "Confirming password does not meet the original password",
+  MIN_LENGTH: (min) => `The minimum length of this field is ${min} characters`,
+  MAX_LENGTH: (max) => `The maximum length of this field is ${max} characters`,
 };
